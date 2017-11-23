bf4a5c158a744f915774305f7675eaef47d26e49
LAPTOP-7QQ4KURV\Sangameswaran <blackdrafteaglekrish@gmail.com>
2017-11-24 00:39:04 +0530

    Some more REST API's

--- app/src/main/java/com/example/sangameswaran/udhira/MainActivity.java
+++ app/src/main/java/com/example/sangameswaran/udhira/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sangameswaran.udhira;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -36,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     List<DonationCentreEntity> donationCentreEntities;
     public GoogleMap map;
     public List<LatLng> bounds;
+    FloatingActionButton fab;
 
     @Override
     public void onBackPressed() {
@@ -50,22 +52,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    fab.setVisibility(View.GONE);
                     mapContainerLL.setVisibility(View.GONE);
                     contentMain.setVisibility(View.VISIBLE);
                     DonorRegistrationFragment fragment=new DonorRegistrationFragment();
                     getSupportFragmentManager().beginTransaction().replace(R.id.content_main,fragment).commit();
                     return true;
                 case R.id.navigation_dashboard:
+                    fab.setVisibility(View.VISIBLE);
                     mapContainerLL.setVisibility(View.VISIBLE);
                     contentMain.setVisibility(View.GONE);
                     return true;
                 case R.id.navigation_notifications:
+                    fab.setVisibility(View.GONE);
                     mapContainerLL.setVisibility(View.GONE);
                     contentMain.setVisibility(View.VISIBLE);
                     BloodRequestFragment fragment1=new BloodRequestFragment();
                     getSupportFragmentManager().beginTransaction().replace(R.id.content_main,fragment1).commit();
                     return true;
                 case R.id.admin:
+                    fab.setVisibility(View.GONE);
                     mapContainerLL.setVisibility(View.GONE);
                     contentMain.setVisibility(View.VISIBLE);
                     AdminFragment fragment2=new AdminFragment();
@@ -81,6 +87,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab=(FloatingActionButton)findViewById(R.id.fab);
         contentMain=(RelativeLayout)findViewById(R.id.content_main);
         donationCentreEntities=new ArrayList<>();
         bounds=new ArrayList<>();
@@ -103,6 +110,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
             }
         }, this);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                relativeZoom(bounds);
+            }
+        });
     }
 
     private void plotCentres() {
